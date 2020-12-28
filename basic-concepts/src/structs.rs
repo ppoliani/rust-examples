9abfcbeaff0ca33e989e041ca618d3243d3834ac
ppoliani <ppoliani@gmail.com>
2020-12-28 13:33:11 +0000

    feat: add multimple struct examples

--- basic-concepts/src/structs.rs
+++ basic-concepts/src/structs.rs
@@ -5,6 +5,23 @@ struct User {
   active: bool
 }
 
+fn create_with_shorthand_notation(email:String, username:String) -> User {
+  User {
+    email,
+    username,
+    active: true,
+    sign_in_count: 0
+  }
+}
+
+fn create_from_existing(email:String, username:String, user:User) -> User {
+  User {
+    email,
+    username,
+    ..user
+  }
+}
+
 pub fn create_user() {
   let user1 = User {
     email: String::from("ppoliani@gmail.com"),
@@ -21,4 +38,16 @@ pub fn create_user() {
   };
 
   user2.username = String::from("ppoliani");
+
+  let user3 = create_with_shorthand_notation(
+    String::from("ppoliani@gmail.com"), 
+    String::from("ppoliani")
+  );
+
+  let user4 = create_from_existing(
+    String::from("ppoliani@gmail.com"), 
+    String::from("ppoliani"),
+    user3
+  );
 }
+
